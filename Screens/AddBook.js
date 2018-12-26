938ceac6275b62ab0c03c8a63b2992a1f7979908
Muaadh Almrham <37133238+MuaathAlhaddad@users.noreply.github.com>
2018-12-26 11:33:40 +0800

    update addbook.js

--- Screens/AddBook.js
+++ Screens/AddBook.js
@@ -1,37 +1,86 @@
-import * as React from 'react';
-import { Text, Button, View, StyleSheet } from 'react-native';
-
-import { Card, List } from 'react-native-paper';
-import { Constants } from 'expo';
-
-export default class AddBookScreen extends React.Component {
-  state = {
-    open: false,
-  };
-  render() {
+import React, {Component} from 'react';
+import {Image,StyleSheet, TextInput, Text, View} from 'react-native';
+// import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-material-cards'
+export default class App extends Component {
+  state={
+    BookTitle: 'book title is here', 
+    Description: 'description is here', 
+    BookImage: 'the image of the book should be here',
+    barCode: 'the bar code scanned will be here', 
+    Booknumber: 1245463
+  }
+  render() { 
     return (
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>Add Book Page</Text>
-        <Card>
-        
-        </Card>
+      
+    <View style={styles.container}>
+      <View style={styles.card}>
+        <Image source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png'}} style={styles.img}/>
+        <View style={styles.textContainer}>
+          <Text >
+                  <Text style={{fontWeight: "bold"}}>
+            Title: 
+                  </Text>
+            {this.state.BookTitle}
+          </Text>
+          <Text >
+                  <Text style={{fontWeight: "bold"}}>
+            Number: 
+                  </Text>
+            {this.state.Booknumber}
+          </Text>
+          <Text >
+                  <Text style={{fontWeight: "bold"}}>
+            Description:  
+                  </Text>
+            {this.state.Description}
+          </Text>
+          
       </View>
+      </View>
+
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='BookTitle' onChangeText={(BookTitle)=>this.setState({BookTitle: BookTitle})}/>
+        <TextInput placeholder='Description' onChangeText={(Description)=>this.setState({Description: Description})}/>
+        <TextInput placeholder='barCode' onChangeText={(barCode)=>this.setState({barCode: barCode})}/>
+        <TextInput placeholder='Booknumber' onChangeText={(Booknumber)=>this.setState({Booknumber: Booknumber})}/>
+        <TextInput placeholder='BookImage' onChangeText={(BookImage)=>this.setState({BookImage: BookImage})}/>
+      </View>
+    </View>
+      
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    justifyContent: 'flex-start',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    borderColor: "gold", 
+    borderWidth: 1, 
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
+  card: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    borderWidth: 1, 
+    borderRadius: 10,
+    margin: 10, 
+    marginTop: 100, 
+    marginBottom: 100,
+    borderColor: '#000',
+    justifyContent: "space-evenly"
   },
+  img: {
+    flex: 1,
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    width: 278,
+    // height: 100
+  },
+  textContainer:{
+    margin: 10
+  }, 
+  inputContainer:{ 
+    // flex: 1,
+    borderColor: "gold", 
+    borderWidth: 1}
+
 });
