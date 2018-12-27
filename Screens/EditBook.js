4b3259ec0367e3b262b46f15fb469c242346d10f
Mohammad Hilmi Bin Halid <45318367+hilmihalid@users.noreply.github.com>
2018-12-27 12:21:20 +0800

    Update EditBook.js

--- Screens/EditBook.js
+++ Screens/EditBook.js
@@ -1,24 +1,75 @@
 import * as React from 'react';
-import { Text, Button, View, StyleSheet } from 'react-native';
+import { Text, Button, View, StyleSheet, Image } from 'react-native';
 
 import { Card, List } from 'react-native-paper';
-import { Constants } from 'expo';
+import { Constants , ImagePicker ,  Permissions} from 'expo';
 
-export default class EditBookScreen extends React.Component {
+export default class UploadImageScreen extends React.Component {
   state = {
     open: false,
+    image: " ",
   };
+
   render() {
+    let {image}= this.state;
+
     return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>Edit Book Details Page</Text>
+      
+       <Image source={{ uri: image}} style={styles.imgCircle}/>
+        <Text style={styles.paragraph}>Add Book Page</Text>
         <Card>
-
-        
+        <Button title="Take a picture" 
+      onPress={this.btmCameraClicked}/>
+      <Text></Text>
+      <Button title="Gallery"
+       onPress={this.btmGalleryClicked}/>
+       <Text></Text>
+       
+         <Button
+            title="Using bar code"
+            onPress={() => this.props.navigation.navigate('Barcode')}
+          />
+          <Text></Text>
+          <Text></Text>
+          <Button
+            title="next"
+            onPress={() => this.props.navigation.navigate('Home')}
+          />
         </Card>
+        <Text></Text>
+
+      
       </View>
     );
   }
+
+  askPermissionAsync = async ()=>{
+    await Permissions.askAsync (Permissions.CAMERA);
+    await Permissions.askAsync(Permissions.CAMERA_ROLL);
+   };
+
+   btmGalleryClicked= async ()=> {
+    await this.askPermissionAsync();
+    let result = await ImagePicker.launchImageLibraryAsync();
+    if (!result.cancelled){
+      this.setState({ image : result.uri});
+
+    }
+   };
+
+
+   btmCameraClicked= async ()=> {
+    await this.askPermissionAsync();
+    let result = await ImagePicker.launchCameraAsync(
+      {allowsEditing: true,
+       aspect:[4, 3] }
+    );
+    if (!result.cancelled){
+      this.setState({ image : result.uri});
+
+    }
+   };
 }
 
 const styles = StyleSheet.create({
@@ -35,4 +86,11 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
+  imgCircle:{
+      width:200,
+      height:200,
+      borderRadius:200/2
+
+  },
 });
+
